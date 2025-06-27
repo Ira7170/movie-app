c5c8480aa9c438f9930d10ce64a6138e0a7edb2b
IraVien <awakenhuman@abv.bg>
2025-06-27 15:51:36 +0300

    Refactor: Move MovieDashboard to pages/, update App.tsx and Home.tsx routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
 import MovieDetail from './Pages/MovieDetail';
+import MovieDashboard  from './Pages/MovieDashboard/MovieDashboard';
 
 const App: React.FC = () => {
   return (
