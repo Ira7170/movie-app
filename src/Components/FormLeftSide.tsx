d8a52e3f71db84b75e5fad990c2a20a4ad036a50
tsvety_tr <ttroshanova@gmail.com>
2025-06-28 06:47:31 +0300

    adding FormLeftSide.tsx

---
+++ src/Components/FormLeftSide.tsx
@@ -0,0 +1,15 @@
+import figure_accent from '../assets/figure_accent.svg'
+
+const FormLeftSide = () => {
+  return (
+        <div className="left-side">
+            <div className="left-side-inner">
+                <img src={figure_accent} alt="Figure Accent" />
+                <h1>Got <br/>
+                cha.</h1>
+            </div>
+        </div>
+  )
+}
+
+export default FormLeftSide
